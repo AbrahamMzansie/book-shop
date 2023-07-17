8f28d08adec86cc0da1c28f68fda9475640718f0
Abraham <abraham.nkomo@omnistall.com>
2023-07-17 13:26:43 +0200

    completed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import BookCreate from "./components/BookCreate";
-import BookEdit from "./components/BookEdit";
 import BookList from "./components/BookList";
 
 import './App.css';
 function App() {
   const [bookList, setBookList] = useState([]);
 
-  const createBookHandler = (bookTitle) => {
-    const newBookData = {
-      id: Math.floor(Math.random() * (100 - 1) + 1),
-      title: bookTitle,
-    };
-    setBookList([...bookList, newBookData]);
+
+  useEffect(() => {
+    retriveAllBooks();
+  }, []);
+
+  const retriveAllBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBookList(response.data)
   }
 
-  const onDeleteBookHandler = (id) => {
+  const createBookHandler = async (bookTitle) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title: bookTitle,
+    });
+    setBookList([...bookList, response.data]);
+  }
+
+  const onDeleteBookHandler = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = bookList.filter(item => {
       return item.id !== id;
     });
     setBookList(updatedBooks);
   }
-  const onEditBookHandler = (id , newTitle) => {
+  const onEditBookHandler = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
     const updatedBooks = bookList.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
-      }  
+        return { ...book, ...response.data };
+      }
       return book;
     });
     setBookList(updatedBooks);
@@ -34,8 +47,8 @@ function App() {
 
   return (
     <div className="App">
-      <BookList onEditBookHandler = {onEditBookHandler} 
-       onDeleteBookHandler={onDeleteBookHandler}
+      <BookList onEditBookHandler={onEditBookHandler}
+        onDeleteBookHandler={onDeleteBookHandler}
         bookList={bookList} />
       <BookCreate bookCreate={createBookHandler} />
     </div>
